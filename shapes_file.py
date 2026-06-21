156ef44ca3bb7e0cfc936e459b1afe485a0c9160
nezjuan <juanmartinezat9@gmail.com>
2026-06-22 01:58:05 +0800

    created first part of the shapes file which basically defines the drawables, movements, rotation function, and boundaries

---
+++ shapes_file.py
@@ -0,0 +1,90 @@
+from copy import deepcopy
+from random import choice, randrange
+from mixins_file import Drawable, Movable, Rotatable, Collidable
+import pygame
+
+class Block:
+    __slots__ = ("_x","_y")
+
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+    
+    @x.setter
+    def x(self, value):
+        self._x = value
+
+    @property
+    def y(self):
+        return self._y
+    
+    @y.settter
+    def y(self, value):
+        self._y = value
+
+    def __repr__(self):
+        return f"Block({self._x}, {self._y})"
+    
+class Figure(Drawable, Movable, Rotatable, Collidable):
+    SHAPE = []
+
+    def __init__(self, tile_size, spawn-(0,0), color= None):
+        if not self.SHAPE:
+            raise NotImplementedError(f"{self.__class__.__name__}must define a non-empty SHAPE")
+        self._tile = tile_size
+        sx, sy = spawn
+        self._blocks = [Block(x + sx, y + sy) for x, y in self.SHAPE]
+        self._color = color or self._random_color()
+
+    @staticmethod
+    def _random_color():
+        return (randrange(30, 256), randrange(30, 256), randrange(30, 256))
+    
+    @property
+    def color(self):
+        return self._color
+    
+    @property
+    def blocks(self):
+        return self._blocks
+    
+    def move (self, dx, dy):
+        for block in self._blocks:
+            block.x += dx
+            block.y += dy
+
+    def rotate(self):
+        center = self._blocks[0]
+        for block in self._blocks:
+            x = block.y - center.y
+            y = block.x - center.x
+            block.x = center.x - x
+            block.y = center.y + y
+
+    def collides(self, field):
+        for block in self._blocks:
+            if block.x < 0 or block.x > field.width - 1:
+                return True
+            if block.y > field.height -1:
+                return True
+            if block.y >= 0 and field.cell(block.x, block.y):
+                return True
+        return False
+    
+    def draw(self, surface, offset =(0, 0)):
+        for block in self.blocks:
+            rect = pygame.Rect(block.x * self._tile + offset[0],
+                block.y * self._tile + offset[1],
+                self._tile -2,
+                self._tile -2)
+            pygame.draw.rect(surface, self._color,rect)
+
+    def clone(self):
+        return deepcopy(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}{self.blocks}"
